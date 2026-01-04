c13758f93cbc486310a347dc49e01439a2d82fc3
moon API <voidddddlolz@gmail.com>
2026-01-03 22:22:23 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
 const games = [
   {name: "Subway Surfers", embed: "https://html5games.com/Game/Subway-Surfers/embed"},
-  {name: "Temple Run 2", embed: "https://playpager.com/embed/temple-run-2/index.html"},
-  {name: "Slope", embed: "https://idev.games/embed/slope"},
   {name: "Basketball Stars", embed: "https://html5games.com/Game/Basketball-Stars/embed"},
   {name: "Fireboy and Watergirl", embed: "https://playpager.com/embed/fireboy-watergirl/index.html"},
   {name: "Moto X3M", embed: "https://idev.games/embed/moto-x3m"},
-  {name: "1v1.LOL", embed: "https://crazygames.com/game/1v1lol/embed"},
-  {name: "Shell Shockers", embed: "https://shellshock.io/embed"},
-  {name: "Krunker", embed: "https://krunker.io/embed"},
-  {name: "Paper.io 2", embed: "https://paperio2.com/embed"},
-  // Add 90+ more – get from htmlgames.com (click Embed on game), playpager.com/embed-games, idev.games
-  // Examples:
+  {name: "Slope", embed: "https://idev.games/embed/slope"},
+  {name: "Temple Run 2", embed: "https://playpager.com/embed/temple-run-2/index.html"},
+  {name: "Paper.io 2", embed: "https://playpager.com/embed/paper-io-2/index.html"},
+  {name: "Hole.io", embed: "https://playpager.com/embed/hole-io/index.html"},
   {name: "Among Us", embed: "https://html5games.com/Game/Among-Us/embed"},
-  {name: "Happy Wheels", embed: "https://totaljerkface.com/happy_wheels.php?embed=1"}, // Some have embed param
-  {name: "Run 3", embed: "https://run3unblocked.io/embed"},
-  // Search "free html5 game embed code" for more sources – copy iframe src directly
+  {name: "Vex 5", embed: "https://playpager.com/embed/vex-5/index.html"},
+  {name: "Run 3", embed: "https://playpager.com/embed/run-3/index.html"},
+  {name: "Happy Wheels", embed: "https://html5games.com/Game/Happy-Wheels/embed"},
+  {name: "2048", embed: "https://playpager.com/embed/2048/index.html"},
+  {name: "Tetris", embed: "https://html5games.com/Game/Tetris/embed"},
+  {name: "Pac-Man", embed: "https://playpager.com/embed/pacman/index.html"},
+  {name: "Snake", embed: "https://html5games.com/Game/Snake/embed"},
+  {name: "Solitaire", embed: "https://html5games.com/Game/Solitaire/embed"},
+  {name: "Mahjong", embed: "https://html5games.com/Game/Mahjong/embed"},
+  {name: "Bubble Shooter", embed: "https://playpager.com/embed/bubble-shooter/index.html"},
+  {name: "Candy Crush", embed: "https://html5games.com/Game/Candy-Crush/embed"},
+  // Add more: go to htmlgames.com → pick game → click Embed → copy iframe src URL
 ];
 
 const grid = document.getElementById('grid');
@@ -24,7 +29,7 @@ games.forEach(game => {
   card.className = 'card';
   card.innerHTML = `
     <p>${game.name}</p>
-    <iframe src="${game.embed}" allowfullscreen></iframe>
+    <iframe src="${game.embed}" allowfullscreen loading="lazy"></iframe>
   `;
   grid.appendChild(card);
 });
